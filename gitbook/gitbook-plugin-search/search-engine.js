d29e6315f8ebcd3b2e3856daab4deb9fb016bd28
Thomas Steenbergen <tom@steenbe.nl>
2017-06-06 16:40:54 +0200

    Update based on #e5938b5

--- gitbook/gitbook-plugin-search/search-engine.js
+++ gitbook/gitbook-plugin-search/search-engine.js
@@ -3,40 +3,48 @@ require([
     'jquery'
 ], function(gitbook, $) {
     // Global search objects
-    var engine = null;
+    var engine      = null;
     var initialized = false;
+
     // Set a new search engine
     function setEngine(Engine, config) {
         initialized = false;
-        engine = new Engine(config);
+        engine      = new Engine(config);
+
         init(config);
     }
+
     // Initialize search engine with config
     function init(config) {
         if (!engine) throw new Error('No engine set for research. Set an engine using gitbook.research.setEngine(Engine).');
+
         return engine.init(config)
-            .then(function() {
-                initialized = true;
-                gitbook.events.trigger('search.ready');
-            });
+        .then(function() {
+            initialized = true;
+            gitbook.events.trigger('search.ready');
+        });
     }
+
     // Launch search for query q
     function query(q, offset, length) {
         if (!initialized) throw new Error('Search has not been initialized');
         return engine.search(q, offset, length);
     }
+
     // Get stats about search
     function getEngine() {
-        return engine ? engine.name : null;
+        return engine? engine.name : null;
     }
+
     function isInitialized() {
         return initialized;
     }
+
     // Initialize gitbook.search
     gitbook.search = {
-        setEngine: setEngine,
-        getEngine: getEngine,
-        query: query,
+        setEngine:     setEngine,
+        getEngine:     getEngine,
+        query:         query,
         isInitialized: isInitialized
     };
 });
--- gitbook/gitbook-plugin-search/search.js
+++ gitbook/gitbook-plugin-search/search.js
@@ -4,7 +4,9 @@ require([
 ], function(gitbook, $) {
     var MAX_RESULTS = 15;
     var MAX_DESCRIPTION_SIZE = 500;
+
     var usePushState = (typeof history.pushState !== 'undefined');
+
     // DOM Elements
     var $body = $('body');
     var $bookSearchResults;
@@ -13,12 +15,13 @@ require([
     var $searchTitle;
     var $searchResultsCount;
     var $searchQuery;
+
     // Throttle search
     function throttle(fn, wait) {
         var timeout;
+
         return function() {
-            var ctx = this,
-                args = arguments;
+            var ctx = this, args = arguments;
             if (!timeout) {
                 timeout = setTimeout(function() {
                     timeout = null;
@@ -27,79 +30,98 @@ require([
             }
         };
     }
+
     function displayResults(res) {
         $bookSearchResults.addClass('open');
+
         var noResults = res.count == 0;
         $bookSearchResults.toggleClass('no-results', noResults);
+
         // Clear old results
         $searchList.empty();
+
         // Display title for research
         $searchResultsCount.text(res.count);
         $searchQuery.text(res.query);
+
         // Create an <li> element for each result
         res.results.forEach(function(res) {
             var $li = $('<li>', {
                 'class': 'search-results-item'
             });
+
             var $title = $('<h3>');
+
             var $link = $('<a>', {
                 'href': gitbook.state.basePath + '/' + res.url,
                 'text': res.title
             });
+
             var content = res.body.trim();
             if (content.length > MAX_DESCRIPTION_SIZE) {
-                content = content.slice(0, MAX_DESCRIPTION_SIZE).trim() + '...';
+                content = content.slice(0, MAX_DESCRIPTION_SIZE).trim()+'...';
             }
             var $content = $('<p>').html(content);
+
             $link.appendTo($title);
             $title.appendTo($li);
             $content.appendTo($li);
             $li.appendTo($searchList);
         });
     }
+
     function launchSearch(q) {
         // Add class for loading
         $body.addClass('with-search');
         $body.addClass('search-loading');
+
         // Launch search query
         throttle(gitbook.search.query(q, 0, MAX_RESULTS)
-            .then(function(results) {
-                displayResults(results);
-            })
-            .always(function() {
-                $body.removeClass('search-loading');
-            }), 1000);
+        .then(function(results) {
+            displayResults(results);
+        })
+        .always(function() {
+            $body.removeClass('search-loading');
+        }), 1000);
     }
+
     function closeSearch() {
         $body.removeClass('with-search');
         $bookSearchResults.removeClass('open');
     }
+
     function launchSearchFromQueryString() {
         var q = getParameterByName('q');
         if (q && q.length > 0) {
             // Update search input
             $searchInput.val(q);
+
             // Launch search
             launchSearch(q);
         }
     }
+
     function bindSearch() {
         // Bind DOM
-        $searchInput = $('#book-search-input input');
-        $bookSearchResults = $('#book-search-results');
-        $searchList = $bookSearchResults.find('.search-results-list');
-        $searchTitle = $bookSearchResults.find('.search-results-title');
+        $searchInput        = $('#book-search-input input');
+        $bookSearchResults  = $('#book-search-results');
+        $searchList         = $bookSearchResults.find('.search-results-list');
+        $searchTitle        = $bookSearchResults.find('.search-results-title');
         $searchResultsCount = $searchTitle.find('.search-results-count');
-        $searchQuery = $searchTitle.find('.search-query');
+        $searchQuery        = $searchTitle.find('.search-query');
+
         // Launch query based on input content
         function handleUpdate() {
             var q = $searchInput.val();
+
             if (q.length == 0) {
                 closeSearch();
-            } else {
+            }
+            else {
                 launchSearch(q);
             }
         }
+
         // Detect true content change in search input
         // Workaround for IE < 9
         var propertyChangeUnbound = false;
@@ -108,6 +130,7 @@ require([
                 handleUpdate();
             }
         });
+
         // HTML5 (IE9 & others)
         $searchInput.on('input', function(e) {
             // Unbind propertychange event for IE9+
@@ -115,32 +138,37 @@ require([
                 $(this).unbind('propertychange');
                 propertyChangeUnbound = true;
             }
+
             handleUpdate();
         });
+
         // Push to history on blur
         $searchInput.on('blur', function(e) {
             // Update history state
             if (usePushState) {
                 var uri = updateQueryString('q', $(this).val());
-                history.pushState({
-                    path: uri
-                }, null, uri);
+                history.pushState({ path: uri }, null, uri);
             }
         });
     }
+
     gitbook.events.on('page.change', function() {
         bindSearch();
         closeSearch();
+
         // Launch search based on query parameter
         if (gitbook.search.isInitialized()) {
             launchSearchFromQueryString();
         }
     });
+
     gitbook.events.on('search.ready', function() {
         bindSearch();
+
         // Launch search from query param at start
         launchSearchFromQueryString();
     });
+
     function getParameterByName(name) {
         var url = window.location.href;
         name = name.replace(/[\[\]]/g, '\\$&');
@@ -150,11 +178,14 @@ require([
         if (!results[2]) return '';
         return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
+
     function updateQueryString(key, value) {
         value = encodeURIComponent(value);
+
         var url = window.location.href;
         var re = new RegExp('([?&])' + key + '=.*?(&|#|$)(.*)', 'gi'),
             hash;
+
         if (re.test(url)) {
             if (typeof value !== 'undefined' && value !== null)
                 return url.replace(re, '$1' + key + '=' + value + '$2$3');
@@ -165,7 +196,8 @@ require([
                     url += '#' + hash[1];
                 return url;
             }
-        } else {
+        }
+        else {
             if (typeof value !== 'undefined' && value !== null) {
                 var separator = url.indexOf('?') !== -1 ? '&' : '?';
                 hash = url.split('#');
@@ -173,7 +205,8 @@ require([
                 if (typeof hash[1] !== 'undefined' && hash[1] !== null)
                     url += '#' + hash[1];
                 return url;
-            } else
+            }
+            else
                 return url;
         }
     }
